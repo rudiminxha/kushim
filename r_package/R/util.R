074c9510b9f7c331ead340f37d9925448a293ede
rminxha <rminxha@trumid.com>
2016-12-15 17:13:04 -0500

    edits

--- r_package/R/util.R
+++ r_package/R/util.R
@@ -1,36 +1,59 @@
 #' Test
 #' @param none
 #'
-#' @test
+#' @get_info_around_me
 
-test <- function(){
+get_info_around_me <- function(long=40.70823, lat=-74.01052, mile_radius=0.5, type = ''){
   endpoint <- "http://live.dbpedia.org/sparql"
   options <- NULL
   prefix <- c("db","http://dbpedia.org/resource/")
 
-  sparql_prefix <- "PREFIX dbp: <http://dbpedia.org/property/>
-                  PREFIX dc: <http://purl.org/dc/terms/>
-                  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
-
-  q <- paste(sparql_prefix,
-  'SELECT ?actor ?movie ?director ?movie_date
-   WHERE {
-     ?m dc:subject <http://dbpedia.org/resource/Category:American_films> .
-     ?m rdfs:label ?movie .
-     FILTER(LANG(?movie) = "en")
-     ?m dbp:released ?movie_date .
-     FILTER(DATATYPE(?movie_date) = xsd:date)
-     ?m dbp:starring ?a .
-     ?a rdfs:label ?actor .
-     FILTER(LANG(?actor) = "en")
-     ?m dbp:director ?d .
-     ?d rdfs:label ?director .
-     FILTER(LANG(?director) = "en")
-   }')
+  q <- get_search_query(long, lat, mile_radius, type)
 
   res <- SPARQL(endpoint,q,ns=prefix,extra=options)$results
 
   res
 
 }
+
+
+
+##QUERIES##
+
+get_search_query <- function(long=40.70823, lat=-74.01052, mile_radius=0.5, type = ''){
+  #convert miles to degrees with approximation
+  radius <- mile_radius/69
+  long_beg <- long - radius
+  long_end <- long + radius
+  lat_beg <- lat - radius
+  lat_end <- lat + radius
+
+  sparql_prefix <- "PREFIX dbp: <http://dbpedia.org/property/>
+     PREFIX dc: <http://purl.org/dc/terms/>
+     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+     PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
+
+
+  if(type == ''){
+    q <- paste(sparql_prefix,
+      sprintf('SELECT * WHERE {
+      ?s geo:lat ?lat .
+      ?s geo:long ?long .
+      FILTER(?lat > %s && ?lat < %s && ?long > %s && ?long < %s)}
+      ',long_beg, long_end, lat_beg, lat_end)
+  )  
+  return(q)  
+  }
+  
+  q <- paste(sparql_prefix,
+    sprintf('SELECT * WHERE {
+    ?s a dbo:%s .
+    ?s geo:lat ?lat .
+    ?s geo:long ?long .
+    FILTER(?lat > %s && ?lat < %s && ?long > %s && ?long < %s)}
+      ',type, long_beg, long_end, lat_beg, lat_end)
+  )
+  return(q)  
+}
+
+
